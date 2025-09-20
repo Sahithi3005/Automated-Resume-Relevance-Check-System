03ee7237f4c4fb652f5991f5246e7e57ee5bf620
Sahithi3005 <gunapusahithi@gmail.com>
2025-09-20 19:38:20 +0530

    Delete src directory

--- src/src/components/src/components/Loader.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Loader({ size = 6 }) {
-  return (
-    <div className={`inline-block animate-spin rounded-full border-4 border-t-transparent border-gray-300`} style={{ width: `${size}rem`, height: `${size}rem` }} />
-  );
-}
