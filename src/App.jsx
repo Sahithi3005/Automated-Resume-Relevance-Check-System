7d018cb1a4a55a2be9d618e8578424bc13792ed7
Sahithi3005 <gunapusahithi@gmail.com>
2025-09-20 14:05:47 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+
+export default function App() {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
+}
